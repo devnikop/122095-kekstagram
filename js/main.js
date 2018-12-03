145b821cd536aab181956b38ebc87d2d08031210
Nikolai <petrov.petrov.ak@gmail.com>
2018-12-03 11:59:11 +0300

    fix issues

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var COUNT_OF_PICTURES = 25;
+
 var COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -30,102 +32,106 @@ var chooseRandomArrayValue = function (value1) {
   return value1[Math.floor(Math.random() * value1.length)];
 };
 
-var createComment = function (arrayOfCommentText, arrayOfFirstNames) {
+var createComment = function () {
+  var message;
+  if (chooseRandomFromTheRange(1, 2) === 1) {
+    message = chooseRandomArrayValue(COMMENTS);
+  } else {
+    message = chooseRandomArrayValue(COMMENTS) + ' ' + chooseRandomArrayValue(COMMENTS);
+  }
+
   var commentObject = {
     avatar: 'img/avatar-' + chooseRandomFromTheRange(1, 6) + '.svg',
-    message: '',
-    name: chooseRandomArrayValue(arrayOfFirstNames),
-
-    messagesCreate: function () {
-      if (chooseRandomFromTheRange(1, 2) === 1) {
-        this.message = chooseRandomArrayValue(arrayOfCommentText);
-      } else {
-        this.message = chooseRandomArrayValue(arrayOfCommentText) + ' ' + chooseRandomArrayValue(arrayOfCommentText);
-      }
-    }
+    message: message,
+    name: chooseRandomArrayValue(FIRST_NAMES)
   };
-  commentObject.messagesCreate();
   return commentObject;
 };
 
-var generateArrayOfPictures = function (arrayOfCommentText, arrayOfFirstNames) {
+var generateArrayOfPictures = function () {
   var arrayOfPictures = [];
-  var countOfObjects = 25;
-  for (var i = 0; i < countOfObjects; i++) {
+
+  for (var i = 0; i < COUNT_OF_PICTURES; i++) {
+    var countOfComments = chooseRandomFromTheRange(2, 5);
+    var comments = [];
+
+    for (var j = 0; j < countOfComments; j++) {
+      comments[j] = createComment();
+    }
+
     var pictureObject = {
       url: 'photos/' + [i + 1] + '.jpg',
       description: 'Описание фотографии',
       countOfLikes: chooseRandomFromTheRange(15, 200),
-      countOfMessages: chooseRandomFromTheRange(2, 5),
-      comments: [],
-
-      commentsCreate: function () {
-        for (var j = 0; j < this.countOfMessages; j++) {
-          this.comments[j] = createComment(arrayOfCommentText, arrayOfFirstNames);
-        }
-      },
+      countOfComments: countOfComments,
+      comments: comments
     };
-    pictureObject.commentsCreate();
     arrayOfPictures[i] = pictureObject;
   }
   return arrayOfPictures;
 };
 
-var arrayOfPictures = generateArrayOfPictures(COMMENTS, FIRST_NAMES);
-
-
-var generateNodeOfPicture = function (template, substitutionalObject) {
-  var element = template.cloneNode(true);
+var generateNodeOfPicture = function (substitutionalObject) {
+  var element = pictureTemplate.cloneNode(true);
 
   element.querySelector('.picture__img').src = substitutionalObject.url;
   element.querySelector('.picture__likes').textContent = substitutionalObject.countOfLikes;
-  element.querySelector('.picture__comments').textContent = substitutionalObject.countOfMessages;
+  element.querySelector('.picture__comments').textContent = substitutionalObject.countOfComments;
   return element;
 };
 
-var addFragment = function (methodForNodeGenerate, substitutionalObjects, template) {
+var addPicturesInFragment = function (substitutionalObjects) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < substitutionalObjects.length; i++) {
-    fragment.appendChild(methodForNodeGenerate(template, substitutionalObjects[i]));
+    fragment.appendChild(generateNodeOfPicture(substitutionalObjects[i]));
   }
   return fragment;
 };
 
-var drawPictures = function (pictures, ContainerForPictures) {
-  ContainerForPictures.appendChild(pictures);
+var drawPictures = function (pictures) {
+  containerForPictures.appendChild(pictures);
 };
 
-var pictureTemplate = document.querySelector('#picture').content;
-var ContainerForPictures = document.querySelector('.pictures');
-
-drawPictures(addFragment(generateNodeOfPicture, arrayOfPictures, pictureTemplate), ContainerForPictures);
-
-
-var bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
-
-var generateNodeOfComments = function (template, substitutionalObject) {
-  var element = template.cloneNode(true);
+var generateNodeOfComments = function (substitutionalObject) {
+  var element = commentTemplate.cloneNode(true);
 
   element.querySelector('.social__picture').src = substitutionalObject.avatar;
   element.querySelector('.social__text').textContent = substitutionalObject.message;
   return element;
 };
 
-var addBigPicture = function (picture, container) {
+var addBigPictureInFragment = function (substitutionalObject) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < substitutionalObject.length; i++) {
+    fragment.appendChild(generateNodeOfComments(substitutionalObject[i]));
+  }
+  return fragment;
+};
+
+var addBigPicture = function (picture) {
   bigPicture.querySelector('.big-picture__img img').src = picture.url;
   bigPicture.querySelector('.social__caption').textContent = picture.description;
   bigPicture.querySelector('.likes-count').textContent = picture.countOfLikes;
-  bigPicture.querySelector('.comments-count').textContent = picture.countOfMessages;
+  bigPicture.querySelector('.comments-count').textContent = picture.countOfComments;
 
-  container.appendChild(addFragment(generateNodeOfComments, picture.comments, commentTemplate));
+  comentsContainer.appendChild(addBigPictureInFragment(picture.comments));
 };
+
+
+var arrayOfPictures = generateArrayOfPictures();
+
+var pictureTemplate = document.querySelector('#picture').content;
+var containerForPictures = document.querySelector('.pictures');
+
+drawPictures(addPicturesInFragment(arrayOfPictures));
+
+var bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
 
 var commentTemplate = document.querySelector('#comment').content;
 var comentsContainer = document.querySelector('.social__comments');
 
-addBigPicture(arrayOfPictures[0], comentsContainer);
-
+addBigPicture(arrayOfPictures[0]);
 
 document.querySelector('.social__comment-count').classList.add('visually-hidden');
 document.querySelector('.comments-loader').classList.add('visually-hidden');
