44d27362676682f3984f9c8a41dbb15dfd1fc9bc
Nikolai <petrov.petrov.ak@gmail.com>
2018-12-21 16:55:14 +0300

    add missing IIFE

--- js/picture.js
+++ js/picture.js
@@ -1,32 +1,34 @@
 'use strict';
 
-var generateNodeOfPicture = function (pictureElement, picture, bigPictureTemplate) {
-  var onPictureClick = function (evt) {
-    evt.preventDefault();
-    window.preview(bigPictureTemplate, picture);
+(function () {
+  var generateNodeOfPicture = function (pictureElement, picture, bigPictureTemplate) {
+    var onPictureClick = function (evt) {
+      evt.preventDefault();
+      window.preview(bigPictureTemplate, picture);
+    };
+
+    pictureElement.querySelector('.picture__img').src = picture.url;
+    pictureElement.querySelector('.picture__likes').textContent = picture.countOfLikes;
+    pictureElement.querySelector('.picture__comments').textContent = picture.countOfComments;
+
+    pictureElement.querySelector('.picture').addEventListener('click', onPictureClick);
+    return pictureElement;
   };
 
-  pictureElement.querySelector('.picture__img').src = picture.url;
-  pictureElement.querySelector('.picture__likes').textContent = picture.countOfLikes;
-  pictureElement.querySelector('.picture__comments').textContent = picture.countOfComments;
+  var addPicturesInFragment = function (pictures) {
+    var fragment = document.createDocumentFragment();
+    var pictureTemplate = document.querySelector('#picture').content;
+    var bigPictureTemplate = document.querySelector('.big-picture-template').content;
 
-  pictureElement.querySelector('.picture').addEventListener('click', onPictureClick);
-  return pictureElement;
-};
-
-var addPicturesInFragment = function (pictures) {
-  var fragment = document.createDocumentFragment();
-  var pictureTemplate = document.querySelector('#picture').content;
-  var bigPictureTemplate = document.querySelector('.big-picture-template').content;
-
-  for (var i = 0; i < pictures.length; i++) {
-    var pictureElement = pictureTemplate.cloneNode(true);
-    fragment.appendChild(generateNodeOfPicture(pictureElement, pictures[i], bigPictureTemplate));
-  }
-  return fragment;
-};
+    for (var i = 0; i < pictures.length; i++) {
+      var pictureElement = pictureTemplate.cloneNode(true);
+      fragment.appendChild(generateNodeOfPicture(pictureElement, pictures[i], bigPictureTemplate));
+    }
+    return fragment;
+  };
 
 
-var arrayOfPictures = window.data();
-var picturesContainerElement = document.querySelector('.pictures');
-picturesContainerElement.appendChild(addPicturesInFragment(arrayOfPictures));
+  var arrayOfPictures = window.data();
+  var picturesContainerElement = document.querySelector('.pictures');
+  picturesContainerElement.appendChild(addPicturesInFragment(arrayOfPictures));
+})();
